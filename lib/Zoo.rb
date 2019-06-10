6ca3eba514be78d7fc1e36b757455680f1d2e24a
Pascal Rassaby <pasbynumbers@gmail.com>
2019-06-10 03:01:14 +0100

    built starter methods and seeds

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,40 @@
 class Zoo
+    attr_accessor(:location, :name)
+    attr_reader()
+    attr_writer()
+
+    @@all = []
+
+    def initialize(name: , location: )
+        @name = name
+        @location = location
+        @@all << self
+    end
+
+#     A zoo should be initialized with a name and a location, which should both be passed as strings.
+
+# Zoo#location should return the location of the zoo instance.
+
+# Zoo#name should return the name of the zoo instance.
+
+# Zoo.all should return an array of all the zoo instances.
+    def self.all
+        @@all
+    end
+# Zoo#animals should return all the animals that a specific instance of a zoo has.
+
+# Zoo#animal_species should return an array of all the species (as strings) of the
+#  animals in the zoo. However, if you have two dogs, it should only return
+#   one "Dog" string (aka an unique array).
+
+# Zoo#find_by_species should take in an animal's species as an argument and
+#  return an array of all the animals in that zoo, which are of that species.
+
+# Zoo#animal_nicknames should return an array of all the nicknames of animals that
+#  a specific instance of a zoo has.
+
+# Zoo.find_by_location should take in a location as an argument and return an
+#  array of all the zoos within that location.
+
 
 end
--- run.rb
+++ run.rb
@@ -4,6 +4,18 @@ require 'pry'
 
 
 #Test your code here
+anim1 = (species: "Dog", weight: 458, nickname: "Fido", zoo: "Urban Cowfitters")
+anim2 = (species: "Fox", weight: 458, nickname: "Ishmail", zoo: "Urban Cowfitters")
+anim3 = (species: "Toucan", weight: 458, nickname: "Tucky", zoo: "Kong Island")
+anim4 = (species: "Monkey", weight: 458, nickname: "Babsy", zoo: "Kong Island")
+anim5 = (species: "Cow", weight: 458, nickname: "Mootoo", zoo: "Urban Cowfitters")
+anim6 = (species: "Horse", weight: 458, nickname: "Bojack", zoo: "Urban Cowfitters")
+anim7 = (species: "Lion", weight: 458, nickname: "Simba", zoo: "Urban Cowfitters")
+anim8 = (species: "Cow", weight: 458, nickname: "Bubbles", zoo: "Urban Cowfitters")
+anim9 = (species: "Giraffe", weight: 458, nickname: "Bracchio", zoo: "Kong Island")
+anim10 = (species: "Monkey", weight: 458, nickname: "Bilbo", zoo: "Kong Island")
+zoo1 = (name: "Urban Cowfitters", location: "London, UK")
+zoo2 = (name: "Kong Island", location: "California, CA")
 
 
 binding.pry
