d1c051369b367b6f28a07e19cbc3841a6fc7fdc6
ArunaKale <a2nkale@gmail.com>
2019-06-10 17:01:25 +0100

    Done.

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,38 @@
+
 class Animal
+  attr_accessor :weight, :species
+  attr_reader :nickname
+   @@all = []
+  
+  def initialize(weight,nickname,species)
+    @weight = weight 
+    @nickname = nickname
+    @species = species
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end 
+
+  def self.find_by_species(species)
+   @@all.select{|data| species == data.species}
+  end
+  
+  # def nickname
+  #   all.find{|data| data.nickname}
+  # end 
+
+  # def weight
+  #   all.find{|data| data.weight}
+  # end 
+
+  # def species
+  # Animal.all.find{|data| data.species}
+  # end 
+  
+  def zoo
+    Zoo.all 
+  end 
 
 end
--- run.rb
+++ run.rb
@@ -5,6 +5,38 @@ require 'pry'
 
 #Test your code here
 
+zoo1 = Zoo.new("Berkshire-Zoo","Berkshire")
+zoo2 = Zoo.new("Buckingham_zoo","Berkshire")
+zoo3 = Zoo.new("Herforshire-Zoo","Herforshire")
+zoo4 = Zoo.new("Sutton-Zoo","Sutton")
+zoo5 = Zoo.new("Windsor-Zoo","Widsor")
+Zoo.all
+Zoo.find_by_location("Berkshire")
+zoo3.find_by_species("Dog") 
+zoo1.animals 
+zoo4.animal_nicknames
+
+
+
+
+animal1 = Animal.new(12,"tommy1","Dog")
+animal2 = Animal.new(20,"tommy2","Dog")
+animal3 = Animal.new(7,"binny1","Cat")
+animal4 = Animal.new(8,"binny2","Cat")
+animal5 = Animal.new(6,"tommy3","Dog")
+animal6 = Animal.new(12,"Jerry","Rat")
+animal7 = Animal.new(12,"Jerry2","Rat")
+Animal.all
+Animal.find_by_species("Dog")
+animal4.weight
+animal3.zoo
+animal1.nickname
+animal7.species
 
 binding.pry
 puts "done"
+
+
+
+zoo2.animal_species("Cat") #unique method is not working
+
