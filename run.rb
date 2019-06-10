3a93ed4ca88a24b1e9a1fdfecedb64289d24baae
Le H Ngo <seiji@o2.pl>
2019-06-10 13:13:33 +0100

    first finish round

--- run.rb
+++ run.rb
@@ -3,7 +3,13 @@ require_relative "lib/Zoo.rb"
 require 'pry'
 
 
-#Test your code here
+lasvegas = Zoo.new("Lvegas", "Las Vegas")
+london = Zoo.new("Londress", "London")
+london2 = Zoo.new("Londressu", "London")
+
+tom = london.add_animal("Tom", "Cat", 3)
+spark = london.add_animal("Spark", "Cat",2)
+billy = london.add_animal("Billy", "Dog", 10)
 
 
 binding.pry
